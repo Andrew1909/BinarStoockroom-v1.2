6878a199c565bc0d781ada6ef752ba96ee0dca92
Andrew1909 <andey.ryzhov.2002@mail.ru>
2023-10-04 08:36:06 +0300

    first commit

---
+++ Pages/AddDeliveriesPage.xaml.cs
@@ -0,0 +1,198 @@
+﻿using LiveCharts.Wpf;
+using StockroomBinar.BD;
+using StockroomBinar.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StockroomBinar.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddDeliveriesPage.xaml
+    /// </summary>
+    public partial class AddDeliveriesPage : Page
+    {
+
+        string[,] DetalesNeme = new string[99, 2];
+        public DeliveriesProducts deliveriesProducts = new DeliveriesProducts();
+        public Deliveries deliveries = new Deliveries();
+
+        string NameDitaliesID = "";
+        int CountPlastProduct = 0;
+        int CountProductProduct = 0;
+        int sumProduct;
+
+        public AddDeliveriesPage()
+        {
+            InitializeComponent();
+            var a = Connect.bd.IDPlasticProducts.Where(p => p.ID != 0).Count();  //заносим данные ID деталей из пластика из БД в combobox 
+            CountPlastProduct = a;
+            AddNameDitalies.Items.Add("Выберите изделие");
+            for (int j = 1; j <= int.Parse(a.ToString()); j++)
+            {
+                var s = Connect.bd.IDPlasticProducts.Where(p => p.IDInside == j).Count();
+                if (s != 0)
+                {
+                    var a1 = Connect.bd.IDPlasticProducts.First(p => p.IDInside == j);
+                    AddNameDitalies.Items.Add(a1.NameProduct.ToString());
+                }
+            }
+            a = Connect.bd.IDProductsProduction.Where(p => p.ID != 0).Count();  //заносим данные ID деталей из пластика из БД в combobox 
+            CountProductProduct = a;
+            for (int j = 1; j <= int.Parse(a.ToString()); j++)
+            {
+                var s = Connect.bd.IDProductsProduction.Where(p => p.IDInside == j).Count();
+                if (s != 0)
+                {
+                    var a1 = Connect.bd.IDProductsProduction.First(p => p.IDInside == j);
+                    AddNameDitalies.Items.Add(a1.NameProducts.ToString());
+                }
+            }
+            AddNameDitalies.SelectedIndex = 0;
+
+
+            string date = DateTime.Now.ToString("yyyy");
+            AddDate.Text = $"__.__.{date}";
+        }
+
+        private void AddDelivereies_Click(object sender, RoutedEventArgs e)
+        {
+            string date = DateTime.Now.ToString("yyyy");
+            if (AddCustomer == null || AddDate.Text == null || DetalesNeme[0, 0] == null || AddDate.Text== $"__.__.{date}" || AddDate.Text=="") MessageBox.Show("Не все поля заполнены!");
+            else
+            {
+                deliveries.СustomerТame = AddCustomer.Text;
+                deliveries.Date = DateTime.ParseExact(AddDate.Text, "dd.MM.yyyy", null);
+                deliveries.Status = 0;
+                Connect.bd.Deliveries.Add(deliveries);
+                Connect.bd.SaveChanges();
+                int position = 1;
+                int maxID = (Connect.bd.Deliveries.Select(q => q.ID).Max());
+                for (int c = 0; c < 99; c++)
+                {
+                    if (DetalesNeme[c, 0] != null)
+                    {
+                        deliveriesProducts.IDInside = maxID;
+                        deliveriesProducts.CodeDitals = DetalesNeme[c, 0];
+                        deliveriesProducts.ReadyDitals = 0;
+                        deliveriesProducts.NecessaryCountDitals= int.Parse(DetalesNeme[c, 1]);
+                        deliveriesProducts.Status = 0;
+                        deliveriesProducts.NumberPosition = position;
+                        Connect.bd.DeliveriesProducts.Add(deliveriesProducts);
+                        Connect.bd.SaveChanges();
+                        position++;
+                    }
+                }
+                MessageBox.Show("Поставка добавлена!");
+                var objA = Connect.bd.Deliveries.First(p => p.ID == maxID);
+                MyFrame.Navigate(new DeliveresInfoPage(objA));
+            }
+        }
+        public class AddtData
+        {
+            public string CodeDitals { get; set; }
+            public string NeseseryDitales { get; set; }
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            if (AddNececeryCount != null && AddNececeryCount.Text != "")
+            {
+                for (int j = 0; j < 99; j++)
+                {
+                    if (DetalesNeme[j, 0] == null)
+                    {
+                        DetalesNeme[j, 0] = NameDitaliesID;
+                        DetalesNeme[j, 1] = AddNececeryCount.Text;
+                        DeliversProductView.Items.Add(new AddtData { CodeDitals = DetalesNeme[j, 0], NeseseryDitales = DetalesNeme[j, 1] });
+                        break;
+                    }
+                }
+                AddNececeryCount.Text = string.Empty;
+                AddNameDitalies.SelectedIndex = 0;
+            }
+        }
+
+        private void AddCodeDitales_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void AddNameDitalies_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            sumProduct = CountProductProduct + CountPlastProduct+1;
+            string[,] DetalesProduction = new string[sumProduct, 2];
+            int countet = 1;
+            int index2;
+            var objA = Connect.bd.IDPlasticProducts.Where(p => p.ID != 0).Count();
+            var objB = Connect.bd.IDProductsProduction.Where(p => p.ID != 0).Count();
+
+
+            if (objA != 0 && objB != 0)
+            { 
+                for(int j=0;j<=objA; j++)
+                {
+                    if (DetalesProduction[j, 0] == null)
+                    {
+                        DetalesProduction[j, 0] = j.ToString();
+                        DetalesProduction[j, 1] = countet.ToString();
+                        countet++;
+                    }
+                }
+                countet = 1;
+
+                objA++;
+                for (int j = objA; j < sumProduct; j++)
+                {
+                    if (DetalesProduction[j, 0] == null)
+                    {
+                        DetalesProduction[j, 0] = j.ToString();
+                        DetalesProduction[j, 1] = countet.ToString();
+                        countet++;
+
+                    }
+                }
+                int index = AddNameDitalies.SelectedIndex;
+                if (AddNameDitalies.SelectedIndex == index)
+                {
+                    if (index > 0)
+                    {
+                        
+                        if (index <= CountPlastProduct)//данные с этими индексами взяты из таблицы с платиковыми деталями
+                        {
+                            var objC = Connect.bd.IDPlasticProducts.First(p => p.IDInside == index);
+                            NameDitaliesID = objC.NameProduct;
+                        }
+                        if (index > CountPlastProduct)// данные с этими индексами взяты из таблицы с деталями с производства
+                        {
+                            
+
+                            for (int j = 0; j < sumProduct; j++)
+                            {
+                                if (DetalesProduction[j, 0] == index.ToString())
+                                {
+
+                                    index2 = int.Parse(DetalesProduction[j, 1].ToString());
+                                    var objC = Connect.bd.IDProductsProduction.First(p => p.IDInside == index2);
+                                    NameDitaliesID = objC.NameProducts;
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Pages/DeliveresInfoPage.xaml.cs
@@ -0,0 +1,196 @@
+﻿using StockroomBinar.BD;
+using StockroomBinar.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StockroomBinar.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для DeliveresInfoPage.xaml
+    /// </summary>
+    public partial class DeliveresInfoPage : Page
+    {
+        public DeliveriesProducts deliveriesProducts = new DeliveriesProducts();
+        public Deliveries deliveries = new Deliveries();
+        public PlasticProducts plasticProducts = new PlasticProducts();
+        public DitalesProduction ditalesProduction = new DitalesProduction();
+        
+        public DeliveresInfoPage(Deliveries item)
+        {
+            InitializeComponent();
+            CostomerText.Text = item.СustomerТame;
+            DataText.Text = item.Date.Value.Date.ToShortDateString();
+            ProcentText.Text = item.Status.ToString();
+
+                                                                                                                                                                                                                //заполняем заказ из бд
+
+            var CountPosition = Connect.bd.DeliveriesProducts.Where(p => p.IDInside == item.ID).Count();
+            int SumReadyDitales = 0;
+            int SumNeseseryDitales = 0;
+            CountPosition++;
+            for (int j=1; j<CountPosition; j++)
+            {
+                var objA = Connect.bd.DeliveriesProducts.Where(p => p.IDInside == item.ID && p.NumberPosition == j).Count();
+                if (objA != 0)
+                {
+                    var objB = Connect.bd.DeliveriesProducts.First(p => p.IDInside == item.ID && p.NumberPosition == j);
+                    var objC = Connect.bd.PlasticProducts.Where(p => p.ProductTypeID == objB.CodeDitals && p.EngravingStatus > 0).Count();                                                                      //проверяем, в какой таблице находится деатль(платик или с произдовдства)
+                    var objD = Connect.bd.DitalesProduction.Where(p => p.CodeDitales == objB.CodeDitals && p.EngravingStatus > 0).Count();
+                    if (objC != 0)
+                    {
+                                                                                                                                                                                                                //из таблици с пластиковыми изделяим
+                        var objE = Connect.bd.PlasticProducts.First(p => p.ProductTypeID == objB.CodeDitals &&p.EngravingStatus>0);
+                        deliveriesProducts = objB;
+                        plasticProducts = objE;
+                        if (objE.EngravingStatus!=0)
+                        {
+                            for (int n = 0; n < deliveriesProducts.NecessaryCountDitals; n++)
+                            {
+                                if(deliveriesProducts.ReadyDitals == deliveriesProducts.NecessaryCountDitals)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    for(int a = 0; a < objC; a++)
+                                    {
+                                        var objR = Connect.bd.PlasticProducts.Where(p => p.ProductTypeID == objB.CodeDitals && p.EngravingStatus > 0).Count();
+                                        if (objR != 0)
+                                        {
+                                            objE = Connect.bd.PlasticProducts.First(p => p.ProductTypeID == objB.CodeDitals && p.EngravingStatus > 0);
+                                            plasticProducts = objE;
+                                            deliveriesProducts.ReadyDitals = deliveriesProducts.ReadyDitals + 1;
+                                            Connect.bd.SaveChanges();
+                                            plasticProducts.EngravingStatus = plasticProducts.EngravingStatus - 1;
+                                            plasticProducts.CountOnStoock = plasticProducts.CountOnStoock - 1;
+                                            Connect.bd.SaveChanges();
+                                        }   
+                                    }
+                                } 
+                            }  
+                        }
+                    }
+
+                    if (objD != 0)
+                    {
+                                                                                                                                                                                                            //из таблици с изделиями с производства
+                        var objE = Connect.bd.DitalesProduction.First(p => p.CodeDitales == objB.CodeDitals && p.EngravingStatus > 0);
+                        deliveriesProducts = objB;
+                        ditalesProduction = objE;
+                        if (objE.EngravingStatus != 0)
+                        {
+                            for (int n = 0; n < deliveriesProducts.NecessaryCountDitals; n++)
+                            {
+                                if (objE.EngravingStatus != 0)
+                                {
+                                    if (deliveriesProducts.ReadyDitals == deliveriesProducts.NecessaryCountDitals)
+                                    {
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        deliveriesProducts.ReadyDitals = deliveriesProducts.ReadyDitals + 1;
+                                        Connect.bd.SaveChanges();
+                                        ditalesProduction.EngravingStatus = ditalesProduction.EngravingStatus - 1;
+                                        ditalesProduction.CountOnStoock = ditalesProduction.CountOnStoock - 1;
+                                        Connect.bd.SaveChanges();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            deliveries = item;
+            SumNeseseryDitales = 0;
+            SumReadyDitales = 0;
+            var objK = Connect.bd.DeliveriesProducts.Where(p => p.IDInside == item.ID).Count();                                                                                                              //количество позиций в поставке
+            var objL = Connect.bd.DeliveriesProducts.First(p => p.IDInside == item.ID);
+            int ID = objL.ID;
+
+                                                                                                                                                                                                            // считаем провент готовности
+            for (int j = 0; j < objK; j++)
+            {
+                var objE = Connect.bd.DeliveriesProducts.First(p => p.IDInside == item.ID &&p.ID==ID);
+                deliveriesProducts = objE;
+                SumNeseseryDitales = SumNeseseryDitales + int.Parse(deliveriesProducts.NecessaryCountDitals.ToString());
+                SumReadyDitales = SumReadyDitales + int.Parse(deliveriesProducts.ReadyDitals.ToString());
+                ID++;
+            }
+            if (SumNeseseryDitales > 0) deliveries.Status = (SumReadyDitales * 100) / SumNeseseryDitales;
+            else deliveries.Status = 0;
+            Connect.bd.SaveChanges();
+            ProcentText.Text = item.Status.ToString();
+            DeliversInfoView.ItemsSource = Connect.bd.DeliveriesProducts.Where(p => p.IDInside == item.ID).ToList();
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseDeliver_Click(object sender, RoutedEventArgs e)                                                                                                                                     //закрыть поставку
+        {
+            if (deliveries.Status == 100)                                                                                                                                                                     //если выполнена полнрстью
+            {
+                if (MessageBox.Show($"Вы действительно хотите закрыть поставку {deliveries.СustomerТame} ?", "Уведомление", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    var objA = Connect.bd.DeliveriesProducts.Where(p => p.IDInside == deliveries.ID).Count();
+                    for (int j = 0; j < objA; j++)
+                    {
+                        var objB = Connect.bd.DeliveriesProducts.First(p => p.IDInside == deliveries.ID);
+                        Connect.bd.DeliveriesProducts.Remove(objB);
+                        Connect.bd.SaveChanges();
+                    }
+                    Connect.bd.Deliveries.Remove(deliveries);
+                    Connect.bd.SaveChanges();
+                    MyFrame.Navigate(new DeliveresPage());
+                }
+               
+            }
+            if (deliveries.Status < 100)                                                                                                                                                                     //если выполнена не полностью
+            {
+                if (MessageBox.Show($"Вы действительно хотите закрыть поставку {deliveries.СustomerТame} раньше ?", "Уведомление", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    var objA = Connect.bd.DeliveriesProducts.Where(p=>p.IDInside == deliveries.ID).Count();
+                    for(int j = 0; j < objA; j++)
+                    {
+                        var objB= Connect.bd.DeliveriesProducts.First(p=>p.IDInside== deliveries.ID);
+                        Connect.bd.DeliveriesProducts.Remove(objB);
+                        Connect.bd.SaveChanges();
+                    }
+                    Connect.bd.Deliveries.Remove(deliveries);
+                    Connect.bd.SaveChanges();
+                    MyFrame.Navigate(new DeliveresPage());
+                }
+            }
+
+        }
+
+        private void СancelDeliver_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Вы действительно хотите отменить поставку {deliveries.СustomerТame} ?", "Уведомление", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+
+            }
+        }
+    }
+}
